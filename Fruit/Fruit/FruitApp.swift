02957a3eedc30812c4f71c2753e70ef3dfd7d764
sky <anvanc37@gmail.com>
2022-02-19 22:25:34 -0500

    initial setup

---
+++ Fruit/Fruit/FruitApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitApp.swift
+//  Fruit
+//
+//  Created by sky on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
