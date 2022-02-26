e358c677580fec76bf148b5b32bb50dd7ea9e35d
sky <anvanc37@gmail.com>
2022-02-25 23:11:17 -0500

    finish ListView and DetailView

---
+++ Fruit/Fruit/App/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Fruit
+//
+//  Created by sky on 2/19/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    // MARK: - PROPERTIES
+    
+    var fruits: [Fruit] = fruitsData
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(fruits.shuffled()) { item in
+                    NavigationLink(destination: FruitDetailView(fruit: item)) {
+                        FruitRowView(fruit: item)
+                            .padding(.vertical, 4)
+                    }
+                }
+            }
+            .navigationTitle("Fruits")
+        } //: NAVIGATION
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView(fruits: fruitsData)
+            .previewDevice("iPhone 11 Pro")
+    }
+}
